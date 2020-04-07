6adf0fac39cbe902b49f2321eee9d3a3c58b0100
Frank Garcia <garcia1901l@gmail.com>
2020-04-07 13:27:24 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import logo from './logo.svg';
+import './App.css';
+
 import * as serviceWorker from './serviceWorker';
+
+class TarjetaFruta extends React.Component {
+  state = {
+    cantidad: 0
+  }
+
+  agregar = () => this.setState({
+    cantidad: this.state.cantidad + 1
+  })
+
+  quitar = () => this.setState({
+    cantidad: this.state.cantidad - 1
+  })
+
+  limpiar = () => this.setState({
+    cantidad: 0
+  })
+
+  render () {
+    return (
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />     
+        <div>
+        <h3>{ this.props.name }</h3>
+        <div>Cantidad: { this.state.cantidad }</div>
+        <button onClick={this.agregar} > + </button>
+        <button onClick={this.quitar} > - </button>
+        <button onClick={this.limpiar} > Limpiar</button>        
+        <hr/>
+        <p>$ { this.props.price }</p>
+      </div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+     
+    )
+  }
+}
+
+const App = () => (
+  <div>
+    <TarjetaFruta name={'Sandia'} price={5.00} />
+    <TarjetaFruta name={'Naranja'} price={1.50} />
+    <TarjetaFruta name='Kiwi' price={3.30} />
+  </div>
+)
+
+//ReactDOM.render(<App />, document.getElementById('root'))
+
+
 
 ReactDOM.render(
   <React.StrictMode>
